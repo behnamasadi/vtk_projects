22efad762ccef836cb9d91843863e8d950d5c74e
Behnam Asadi <behnam.asadi@gmail.com>
2023-10-13 16:27:38 +0200

    up

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -1,53 +1,46 @@
 #include <vtkActor.h>
+#include <vtkAssembly.h>
+#include <vtkConeSource.h>
+#include <vtkFollower.h>
+#include <vtkImageActor.h>
+#include <vtkImageMapper3D.h>
+#include <vtkInteractorStyleTrackballActor.h>
 #include <vtkLookupTable.h>
+#include <vtkMatrix4x4.h>
 #include <vtkNew.h>
+#include <vtkPNGReader.h>
+#include <vtkPlaneSource.h>
 #include <vtkPolyDataMapper.h>
+#include <vtkProp3DFollower.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
 #include <vtkScalarBarActor.h>
 #include <vtkSphereSource.h>
+#include <vtkTransform.h>
 
 int main(int, char *[]) {
   // Create a simple sphere
   vtkNew<vtkSphereSource> sphereSource;
   sphereSource->Update();
 
+  vtkNew<vtkConeSource> coneSource;
+  coneSource->Update();
+
   // Create a mapper and actor
-  vtkNew<vtkPolyDataMapper> mapper;
-  mapper->SetInputConnection(sphereSource->GetOutputPort());
+  vtkNew<vtkPolyDataMapper> sphereMapper;
+  sphereMapper->SetInputConnection(sphereSource->GetOutputPort());
 
-  const int numStripes = 4;
+  vtkNew<vtkPolyDataMapper> coneMapper;
+  coneMapper->SetInputConnection(coneSource->GetOutputPort());
 
-  // Create a lookup table with alternating black and white stripes
-  vtkNew<vtkLookupTable> lut;
-  lut->SetNumberOfTableValues(numStripes);
-  for (int i = 0; i < numStripes; ++i) {
-    if (i % 2 == 0) {
-      lut->SetTableValue(i, 1.0, 1.0, 1.0, 1.0); // white
-    } else {
-      lut->SetTableValue(i, 0.0, 0.0, 0.0, 1.0); // black
-    }
-  }
-  lut->Build();
+  vtkNew<vtkActor> sphereActor;
+  sphereActor->SetMapper(sphereMapper);
 
-  mapper->SetLookupTable(lut);
+  vtkNew<vtkActor> coneActor;
+  coneActor->SetMapper(coneMapper);
 
-  vtkNew<vtkActor> actor;
-  actor->SetMapper(mapper);
-
-  // Create a scalar bar
-  vtkNew<vtkScalarBarActor> scalarBar;
-  scalarBar->SetLookupTable(mapper->GetLookupTable());
-  scalarBar->SetTitle("My Scalar Bar");
-  scalarBar->SetNumberOfLabels(numStripes);
-  scalarBar->SetOrientationToHorizontal();
-
-  // Position the scalar bar at the bottom of the window
-  scalarBar->GetPositionCoordinate()->SetCoordinateSystemToNormalizedDisplay();
-  scalarBar->GetPositionCoordinate()->SetValue(0.1, 0.05);
-  scalarBar->SetWidth(0.8);
-  scalarBar->SetHeight(0.1);
+  // coneActor->SetPosition(5, 2, 3);
 
   // Create a renderer, render window, and interactor
   vtkNew<vtkRenderer> renderer;
@@ -57,11 +50,92 @@ int main(int, char *[]) {
   renderWindowInteractor->SetRenderWindow(renderWindow);
 
   // Add the actor and scalar bar to the scene
-  renderer->AddActor(actor);
-  renderer->AddActor2D(scalarBar);
+  // renderer->AddActor(sphereActor);
+  // renderer->AddActor(coneActor);
 
-  // Render and interact
+  vtkNew<vtkTransform> transform;
+  vtkNew<vtkMatrix4x4> m;
+
+  m->Identity();
+  m->SetElement(0, 0, .3);
+  m->SetElement(1, 1, .3);
+  m->SetElement(2, 2, .3);
+
+  m->SetElement(0, 3, 10);
+  m->SetElement(1, 3, 3);
+
+  transform->SetMatrix(m);
+  // transform->Scale(5, 5, 5);
+  double scale[3];
+
+  transform->GetScale(scale);
+
+  std::cout << "-----------" << scale[0] << "," << scale[1] << "," << scale[2]
+            << std::endl;
+
+  sphereActor->GlobalWarningDisplayOn();
+  // sphereActor->SetUserTransform(transform);
+
+  // vtkNew<renderWindowInteractor> i_ren;
+
+  // // actor->SetUserMatrix(m);
+
+  // // Render and interact
+
+  /*
+    vtkNew<vtkPNGReader> pnmReader;
+    pnmReader->SetFileName("../../images/maps-marker.png");
+
+    vtkNew<vtkImageActor> ia;
+    ia->GetMapper()->SetInputConnection(pnmReader->GetOutputPort());
+    ia->SetScale(0.01, 0.01, 0.01);
+
+    vtkNew<vtkProp3DFollower> p3dFollower;
+
+    p3dFollower->SetProp3D(ia);
+    p3dFollower->SetCamera(renderer->GetActiveCamera());
+
+    vtkNew<vtkAssembly> assembly;
+    assembly->AddPart(sphereActor);
+    assembly->AddPart(coneActor);
+    assembly->AddPart(p3dFollower);
+
+    renderer->AddActor(assembly);
+    // renderer->AddActor(p3dFollower);
+
+    renderWindow->Render();
+
+    */
+
+  vtkNew<vtkPNGReader> pnmReader;
+  pnmReader->SetFileName("../../images/maps-marker.png");
+
+  vtkNew<vtkImageActor> ia;
+  ia->GetMapper()->SetInputConnection(pnmReader->GetOutputPort());
+  ia->SetScale(0.01, 0.01, 0.01);
+
+  vtkNew<vtkProp3DFollower> p3dFollower;
+
+  p3dFollower->SetProp3D(ia);
+  p3dFollower->SetCamera(renderer->GetActiveCamera());
+
+  vtkNew<vtkAssembly> assembly;
+  assembly->AddPart(sphereActor);
+  assembly->AddPart(coneActor);
+
+  renderer->AddActor(assembly);
+  renderer->AddActor(p3dFollower); // Add the follower directly to the renderer
+
+  assembly->SetUserMatrix(m);
+
+  // Ensure the camera settings and update the view
+  renderer->ResetCamera();
   renderWindow->Render();
+
+  vtkNew<vtkInteractorStyleTrackballActor> style;
+
+  // renderWindowInteractor->SetInteractorStyle(style);
+
   renderWindowInteractor->Start();
 
   return EXIT_SUCCESS;
